3a68d2ea227b3952b0a68a63ae49cc65aa3712a6
NataliiaBoichuk <natalia.boichuk@qatestlab.eu>
2021-10-13 15:28:15 +0300

    add example

--- aнонімні функції/lambda.py
+++ aнонімні функції/lambda.py
@@ -10,3 +10,12 @@
 # Синтаксический формат
 # lambda [arg1 [,arg2,.....argn]]:expression
 
+# this will return sum of two elements
+s = (lambda x, y: x + y)(2, 3)
+
+# out will be list where each element is multiplied by 2
+o = list(map(lambda n: n * 2, [1, 2, 3, 4, 5]))
+
+# each element in list strs will be filtered by len, if len > 3 than this element will be add to new list
+strs = ['apple', 'and', 'a', 'donut']
+f = list(filter(lambda s: len(s) > 3, strs))
